00f838f4f9c502744167838a8259adb391c4155f
umbrella <13798972826@163.com>
2019-07-26 23:46:40 +0800

    1.1.2

--- jQ-小列表todolist/common/js/base.js
+++ jQ-小列表todolist/common/js/base.js
@@ -3,14 +3,15 @@
 
     $(function(){
         var $form_add_task = $('.add-task').children(".btn")
+        , $form_input = $('.add-task').children('input')
         , $task_delect_trigger
         , $task_detail_trigger
         , $task_detail = $('.task-detail')
         , $task_detail_mask = $('.task-detail-mask')
         , task_list = []
-        , isclick = false
         , current_index
         , $update_form
+        , $task_detail_content
         ;
 
     init();
@@ -18,28 +19,40 @@
     /* 初始化localStorage数据 */
     function init(){
         task_list = store.get('task_list') || []; 
-        console.log(' task_list111', task_list);
         if(task_list.length) 
         render_task_list();
      }
 
     /* 监听输入框提交按钮的点击事件 */
-    $form_add_task.on('click', function(){;
+    $form_add_task.on('click', add_task_fn);
+    $form_input.on('keydown',function(e){
+        console.log('111')
+        var e = e || window.event;
+        if(e.keyCode == 13){
+            console.log('222')
+            add_task_fn();
+        }
+    })
+
+      function add_task_fn(){
         var new_task = {}, $inputVal;
+        console.log("++++111+++");
        
         // 获取新Task的值
-        $inputVal = $(this).siblings('input[type=text]');
+        $inputVal = $form_input;
         new_task.content = $inputVal.val();
 
         // 如果Task值为空，则直接返回 否则继续执行
+        console.log("++++222+++");
         if(!new_task.content &&  new_task.content == '') return;
+        console.log("+++333++++");
         
         if(add_task(new_task)){
             // 清空输入框的内容
         $inputVal.val(null);
         
         }
-    })
+    }
 
     /* 查找并监听task删除按钮的点击事件 */
     function listen_task_delete(){
@@ -48,7 +61,7 @@
 
         // 找到删除按钮对应的task元素
         var $item = $this.parent().parent();
-        var index = $item.data('index');
+        var index = Number($item.data('index'));
 
         // 确认删除询问
         var pop = confirm('确认删除吗?');
@@ -61,8 +74,8 @@
         $task_detail_trigger.on('click',function(){
             var $this = $(this);
             var $item = $this.parent().parent();
-            var index = $item.data('index');
-            console.log('index',index);
+            var index = Number($item.data('index'));
+
             show_task_detail(index);
     })
     }
@@ -80,45 +93,61 @@
             var data = {};
             data.content = $(this).find('[name=content]').val();
             data.desc = $(this).find('[name=desc]').val();
-            data.remind_data = $(this).find('[name=remind_data]').val();
-
-            console.log('data',data);
+            data.remind_data = $(this).find('[name=remind_date]').val();
 
             update_task(index,data);
         })
     }
 
     function update_task(index,data){
-        if(!index) return;
+        if(!index || !task_list[index]) return;
         task_list[index] = data;
-        console.log('task_list[index]',task_list[index]);
-        console.log('store.get(task_list)',store.get('task_list'));
         refresh_task_list();
     }
 
     /* 渲染指定task对应详情框的数据 */
     function render_task_detail(index){
-        if( index === undefined) return;
-        console.log('task_list[index]',task_list[index]);
+        if( index === undefined || !task_list[index]) return;
         var item = task_list[index];
-        console.log('item',item);
+
+        console.log("item",item);
         var detail_content = 
             '<form>'  +
             '<div class="item-content">'  +
-            
+            item.content + 
             '</div>'  +
+            '<div>' +
+            '<input style="display: none;" type="text" name="content" value="'+ item.desc +'">' +
+            '</div>' +
             '<div class="item-desc">'  +
-            '<textarea name="desc"></textarea>'  +
+            '<textarea name="desc">' + item.desc + '</textarea>'  +
             '</div>'  +
             '<div class="item-remind">' +
-            '<input name="remind_data" type="date">'  +
+            '<input name="remind_date" type="date">'  +
             '</div>' +
             '<div><button type="submit">更新</button></div>' + 
             '</form>' ;
         $task_detail.html(null);
         $task_detail.html(detail_content);
+        $update_form = $task_detail.find('form');
+        console.log("$update_form",$update_form)
+        $task_detail_content = $update_form.find('.item-content');
+        console.log("$task_detail_content",$task_detail_content)
 
+        $task_detail_content.on('dblclcik',function(){
+            console.log('111');
+        })
 
+        $update_form.on('submit',function(e){
+            console.log("kkkk") 
+            e.preventDefault();
+            var data = {};
+            data.content = $(this).find('[name = content]').val();
+            data.desc = $(this).find('[name = desc]').val();
+            data.remind_date = $(this).find('[name = remind_date]').val();
+
+            update_task(index, data);
+        })
     }
 
     $task_detail_mask.on('click', hide_task_detail);
@@ -129,8 +158,9 @@
         $task_detail_mask.hide();
     }
 
-    /* 添加清单条目 */
+    /* 添加清单条目 */ 
     function add_task(new_task){
+        console.log("++++444+++",new_task);
 
         // 将新Task推入task_list
         task_list.push(new_task);
@@ -142,6 +172,7 @@
 
     /* 刷新localStorage数据并且更新页面 */
     function refresh_task_list(){
+        console.log("+++555++++",task_list);
         store.set('task_list',task_list);       
         render_task_list();
     } 
@@ -151,18 +182,17 @@
     function delete_task(index){
         
         // 如果没有index或者不存在则直接return
-        if( index === undefined) return;
+        if( index === undefined || !task_list[index]) return;
         delete task_list[index];
         refresh_task_list()
     }
 
     /* 渲染全部Task模板 */
     function render_task_list(){
+        console.log("++++666+++");
         var $task_list = $('.task-list');
-        if(isclick){
         $task_list.html('');
-        }
-        isclick = true;
+
         // 循环调用渲染单条Task模板（调用）
         for(var i = 0; i < task_list.length; i++){
             var $task_item = render_list_item(task_list[i],i);
@@ -178,7 +208,9 @@
 
     /* 渲染单条Task模板（方法） */
     function render_list_item(data, index){
+        console.log("+++777++++");
         if(!data) return;
+        console.log("+++888++++");
          var list_item = 
             '<div class="task-item" data-index=" ' + index + '">' + 
             '<span><input type="checkbox"></span>' + 
